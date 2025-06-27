675ba67dd2b5412636a73524761046faaffd8384
DorianABDS <dorian.abbadessa@laplateforme.io>
2025-06-27 16:52:22 +0200

    feat: add data file and implement file reading functionality

---
+++ exercises/job6/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+try {
+  const data = fs.readFileSync('./data.txt', 'utf8');
+  console.log("Contenu du fichier data.txt: ", data);
+} catch (err) {
+  console.error(err);
+}
